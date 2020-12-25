3aeb7fbb1fbea5ba5ddd1d92504cd685543ad7c0
ShiyuXu <ShiyuXu99@users.noreply.github.com>
2020-12-24 21:57:59 -0500

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <Router>
-
         <div>
           <Route path="/" exact component={SignIn}/>
           <Route path="/Home" component={Home}/>
